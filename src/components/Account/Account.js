c533fcacab7596c4de8c8c24864daf16959932ed
Jamie95187 <jamiewong63@googlemail.com>
2020-05-20 22:47:29 +0100

    create Account page

---
+++ src/components/Account/Account.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Button from '../UI/Button/Button';
+
+class Account extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Accounts Page</h1>
+        <Button>Create Team</Button>
+      </div>
+    )
+  }
+}
