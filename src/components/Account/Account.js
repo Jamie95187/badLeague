1d57e65b831e6facec1ee3d83dfbe92b4a2d21e6
Jamie95187 <jamiewong63@googlemail.com>
2020-05-22 00:42:49 +0100

    render modal when building a team (completing form)

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Button from '../UI/Button/Button';
+import TeamBuilder from '../../containers/TeamBuilder/TeamBuilder';
+import Aux from '../../hoc/Aux';
 
 class Account extends Component {
+  state = {
+    building: false
+  }
   render(){
     return (
-      <div>
+      <Aux>
+        <Modal show={true}>
+          {TeamBuilder}
+        </Modal>
         <h1>Accounts Page</h1>
-        <Button>Create Team</Button>
-      </div>
+        <Button
+          clicked={this.}>Create Team</Button>
+      </Aux>
     )
   }
 }
