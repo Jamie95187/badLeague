f7351e97a163fe33d8e17d79acad1e568a5b0000
Jamie95187 <jamiewong63@googlemail.com>
2020-05-23 23:33:17 +0100

    modal component mounted

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import classes from './Modal.css';
+import './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
@@ -14,7 +14,7 @@ class Modal extends Component {
       <Aux>
         <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
         <div
-          className={classes.Modal}
+          className={"Modal"}
           style={{
             transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
             opacity: this.props.show ? '1' : '0'
