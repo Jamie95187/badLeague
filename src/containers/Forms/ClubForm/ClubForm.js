9458967255ed33fe89884f4708bca375c5d1743e
Jamie <jamiewong63@googlemail.com>
2020-07-19 21:38:47 +0100

    form for clubs

--- src/containers/Forms/ClubForm/ClubForm.js
+++ src/containers/Forms/ClubForm/ClubForm.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 import Input from '../../Input/Input';
 import Button from '../../../components/UI/Button/Button'
+import axios from '../../../axios-teams';
 
 // Similar to Contact Data in the Burger App
 
 class ClubForm extends Component {
-  
+
+  state = {
+    clubForm: {
+        name: {
+          elementType: 'input',
+          elementConfig: {
+            type: 'text',
+            placeholder: 'Your Club\'s Name'
+          },
+          value: '',
+          // validation: {
+          //   required: true
+          // },
+          // valid: false,
+          // touched: false
+        },
+        email: {
+          elementType: 'input',
+          elementConfig: {
+            type: 'text',
+            placeholder: 'Contact Email'
+          },
+          value: '',
+          // validation: {
+          //   required: true
+          // },
+          // valid: false,
+          // touched: false
+        },
+        phone: {
+          elementType: 'input',
+          elementConfig: {
+            type: 'text',
+            placeholder: 'Contact Telephone Number'
+          },
+          value: '',
+        },
+        description: {
+          elemetntType: 'input',
+          elementConfig: {
+            type: 'text',
+            placeholder: 'Any relevant information (eg club nights, venue)'
+          },
+          value: '',
+        }
+      }
+  }
+
   render(){
     return (
       <form>
